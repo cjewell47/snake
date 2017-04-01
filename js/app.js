34492c4939258867395109242b901d44ac44622a
cjewell47 <charles.jewell47@gmail.com>
2017-04-01 19:54:38 +0100

    loop not quite working

--- js/app.js
+++ js/app.js
@@ -9,31 +9,92 @@
 
 // cells have a background color on a timer that fades out.
 
-(() => {
-
-  function start() {
-
-    console.log('yo');
-
-    // $('.row4 .col4').attr('id', 'snake');
-
-  }
+// (() => {
 
 
-  $(document).bind('keyup', function (e){
-    if (e.which===37) {
-      $(start);
-    }
-    if (e.which===38) {
-      $(start);
-    }
-    if (e.which===39) {
-      $(start);
-    }
-    if (e.which===40) {
-      $(start);
+
+
+  // $(document).keyup(function(e) {
+  //   // let speedLeft;
+  //   // let speedUp;
+  //   // let speedRight;
+  //   // let speedDown;
+  //   switch(e.which) {
+  //     case 37: // left
+  //       console.log('left');
+  //       $('#grid').find('.cell#head')
+  //       .animate({backgroundColor: '#000'}, '0.6')
+  //       .animate({backgroundColor: '#fff'}, '1.2');
+  //       setTimeout(notHead, 650);
+  //       speedLeft = setInterval( function() {
+  //         // let leftColn = $('#grid').find('div#head').attr('data-col');
+  //         // let leftCol = (leftColn - 1).toString();
+  //         // let leftRown = $('#grid').find('div#head').attr('data-row');
+  //         // let leftRow = leftRown.toString();
+  //         // $(`.cell[data-row=${leftRow}][data-col=${leftCol}]`)
+  //         // .attr('id', 'head')
+  //         // .animate({backgroundColor: '#000'}, '0.6')
+  //         // .animate({backgroundColor: '#fff'}, '1.2');
+  //         // setTimeout(notHead, 650);
+  //         console.log('yo');
+  //       }, 600);
+  //       break;
+  //     case 38: // up
+  //       console.log('up');
+  //       // $('#grid').find('div.row8.col8')
+  //       // .animate({backgroundColor: '#000'}, '0.6')
+  //       // .animate({backgroundColor: '#fff'}, '1.2');
+  //       break;
+  //     case 39: // right
+  //       console.log('right');
+  //       // $('#grid').find('div.row8.col8')
+  //       // .animate({backgroundColor: '#000'}, '0.6')
+  //       // .animate({backgroundColor: '#fff'}, '1.2');
+  //       break;
+  //     case 40: // down
+  //       console.log('down');
+  //       // $('#grid').find('div.row8.col8')
+  //       // .animate({backgroundColor: '#000'}, '0.9')
+  //       // .animate({backgroundColor: '#fff'}, '0.9');
+  //       break;
+  //     default: return; // exit this handler for other keys
+  //   }
+  //   e.preventDefault();
+  // });
+
+  $(document).one('keyup', function(e) {
+    if(e.which===13) {
+      console.log('sup');
+      $('#grid').find('.cell.head')
+      .animate({backgroundColor: '#000'}, '0.6')
+      .animate({backgroundColor: '#fff'}, '1.2');
+      setTimeout(notHead, 650);
+    //   setInterval(() => {
+    // //     // let rightColn = $('#grid').find('.cell.head').attr('data-col');
+    // //     // let rightCol = (rightColn + 1).toString();
+    // //     // let rightRown = $('#grid').find('.cell.head').attr('data-row');
+    // //     // let rightRow = (rightRown).toString();
+    // //     // $(`.cell[data-row=${rightRow}][data-col=${rightCol}]`)
+    // //     // .addClass('head')
+    // //     // .animate({backgroundColor: '#000'}, '0.6')
+    // //     // .animate({backgroundColor: '#fff'}, '1.2');
+    // //     // setTimeout(notHead, 600);
+    // //     var colNum = $('#grid').find('.cell.head').attr('data-col');
+    // //     var rowNum = $('#grid').find('.cell.head').attr('data-row');
+    // //     var prevCell = $(`.cell[data-row="${rowNum}"][data-col="${colNum}"]`);
+    // //     setTimeout(prevCell.addClass('head'), 200);
+    // //     prevCell.animate({backgroundColor: '#000'}, '0.6')
+    // //     .animate({backgroundColor: '#fff'}, '1.2');
+    // //     setTimeout(notHead, 800);
+    //   }, 600);
+
+      e.preventDefault();
     }
 
   });
 
-});
+  function notHead() {
+    $('.cell.head').removeClass('head');
+  }
+
+// });
