abcc2de7a1fc66a382d10149b6abf16313127fea
cjewell47 <charles.jewell47@gmail.com>
2017-04-03 10:45:06 +0100

    Snake can move and turn

--- js/app.js
+++ js/app.js
@@ -9,95 +9,75 @@
 
 // cells have a background color on a timer that fades out.
 
-// (() => {
+$(init);
 
+function init() {
+  buildGrid();
+  let direction = chooseRandomDirection();
+  let nextIndex = chooseRandomIndex();
 
+  setInterval(() => {
+    // Remove duplication
+    const width      = 40;
+    const $start     = $($('li')[nextIndex]);
 
+    $start.addClass('snake').delay(1000).queue(function() {
+      $(this).removeClass('snake').dequeue();
+    });
 
-  // $(document).keyup(function(e) {
-  //   // let speedLeft;
-  //   // let speedUp;
-  //   // let speedRight;
-  //   // let speedDown;
-  //   switch(e.which) {
-  //     case 37: // left
-  //       console.log('left');
-  //       $('#grid').find('.cell#head')
-  //       .animate({backgroundColor: '#000'}, '0.6')
-  //       .animate({backgroundColor: '#fff'}, '1.2');
-  //       setTimeout(notHead, 650);
-  //       speedLeft = setInterval( function() {
-  //         // let leftColn = $('#grid').find('div#head').attr('data-col');
-  //         // let leftCol = (leftColn - 1).toString();
-  //         // let leftRown = $('#grid').find('div#head').attr('data-row');
-  //         // let leftRow = leftRown.toString();
-  //         // $(`.cell[data-row=${leftRow}][data-col=${leftCol}]`)
-  //         // .attr('id', 'head')
-  //         // .animate({backgroundColor: '#000'}, '0.6')
-  //         // .animate({backgroundColor: '#fff'}, '1.2');
-  //         // setTimeout(notHead, 650);
-  //         console.log('yo');
-  //       }, 600);
-  //       break;
-  //     case 38: // up
-  //       console.log('up');
-  //       // $('#grid').find('div.row8.col8')
-  //       // .animate({backgroundColor: '#000'}, '0.6')
-  //       // .animate({backgroundColor: '#fff'}, '1.2');
-  //       break;
-  //     case 39: // right
-  //       console.log('right');
-  //       // $('#grid').find('div.row8.col8')
-  //       // .animate({backgroundColor: '#000'}, '0.6')
-  //       // .animate({backgroundColor: '#fff'}, '1.2');
-  //       break;
-  //     case 40: // down
-  //       console.log('down');
-  //       // $('#grid').find('div.row8.col8')
-  //       // .animate({backgroundColor: '#000'}, '0.9')
-  //       // .animate({backgroundColor: '#fff'}, '0.9');
-  //       break;
-  //     default: return; // exit this handler for other keys
-  //   }
-  //   e.preventDefault();
-  // });
-
-  $(document).keyup(function(e) {
-    if(e.which===13) {
-      console.log('hey');
-      $('#grid').find('.cell.head')
-      .animate({backgroundColor: '#000'}, '0.6')
-      .animate({backgroundColor: '#fff'}, '1.2');
-      setTimeout(notHead, 650);
-      setInterval(function() {
-    //     // let rightColn = $('#grid').find('.cell.head').attr('data-col');
-    //     // let rightCol = (rightColn + 1).toString();
-    //     // let rightRown = $('#grid').find('.cell.head').attr('data-row');
-    //     // let rightRow = (rightRown).toString();
-    //     // $(`.cell[data-row=${rightRow}][data-col=${rightCol}]`)
-    //     // .addClass('head')
-    //     // .animate({backgroundColor: '#000'}, '0.6')
-    //     // .animate({backgroundColor: '#fff'}, '1.2');
-    //     // setTimeout(notHead, 600);
-        let colNum = $('#grid').find('.cell.head').attr('data-col');
-        let rowNum = $('#grid').find('.cell.head').attr('data-row');
-        // colNum = colNum.toString();
-        // rowNum = rowNum.toString();
-        // var prevCell = $(`.cell[data-row="${rowNum}"][data-col="${colNum}"]`);
-        let nextCell = $('#grid').find(`.cell[data-row="${rowNum}"][data-col="${colNum}+1"]`);
-        setTimeout(nextCell.addClass('head'), 600);
-        nextCell.animate({backgroundColor: '#000'}, '0.6')
-        .animate({backgroundColor: '#fff'}, '1.2');
-        setTimeout(notHead, 1200);
-      }, 1000);
-
-      e.preventDefault();
+    switch (direction) {
+      case 'N':
+        nextIndex = nextIndex - width;
+        break;
+      case 'E':
+        nextIndex++;
+        break;
+      case 'S':
+        nextIndex = nextIndex + width;
+        break;
+      case 'W':
+        nextIndex--;
+        break;
     }
+  }, 1000);
 
+  $(document).keydown(function(e) {
+    console.log(e.which);
+    switch(e.which) {
+      case 37: // left
+        direction = 'W';
+        break;
+      case 38: // up
+        direction = 'N';
+        break;
+      case 39: // right
+        direction = 'E';
+        break;
+      case 40: // down
+        direction = 'S';
+        break;
+      default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
   });
+}
 
-  function notHead() {
-    $('.cell.head').removeClass('head');
+function buildGrid() {
+  const $body = $('body');
+  const $grid = $('<ul class="grid"></ul>');
+  const width = 40;
+  $body.append($grid);
+  for (let i = 0; i < width*width; i++) {
+    $grid.append('<li></li>');
   }
+}
 
-// });
+function chooseRandomIndex() {
+  const gridArray = $('li');
+  return Math.floor(Math.random() * gridArray.length);
+}
+
+function chooseRandomDirection() {
+  const directions = ['N','E','S','W'];
+  return directions[Math.floor(Math.random() * directions.length)];
+}
